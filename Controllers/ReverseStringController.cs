5856eb4fd9d767e59a87a4c40c6d33180ff756a2
Melissa Gwyn <melissagwyn@gmail.com>
2023-05-24 18:55:42 -0400

    Created Reverse String Controller

---
+++ Controllers/ReverseStringController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExplosionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReverseStringController : ControllerBase
+    {
+        [HttpGet("{str}")]
+        public string Reverse(string str)
+        {
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
